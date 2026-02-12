3dd32a5aa4384e7b92b309c3dbce0a1847e7a6f9
skarabeiy <skarabeiy@mail.ru>
2026-02-12 17:12:08 +0300

    update: make order and user services very simple

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.example.orderservice.controller;
 
-import com.example.orderservice.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
 
-    private final OrderService orderService;
-
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
-
     @GetMapping("/test")
     public String test() {
         return "Hello from Order Service!";
-    }
-
-    @GetMapping("/test-feign")
-    public String testFeign() {
-        return orderService.testFeign();
     }
 }
